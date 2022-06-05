4e2ce0d0219979d3c007fad111b5157b6cb9c266
Brandon Northrop <brannorth0414@yahoo.com>
2022-06-05 10:33:54 -0600

    Initial config-client properties

---
+++ src/main/java/com/quintrix/jfs/quintrixcloudserver/QuintrixCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintrixCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintrixCloudServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quintrix/jfs/quintrixcloudserver/QuintrixCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixcloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuintrixCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
